de0cc19e1c775c2c684028609cc96d5c67e35dda
Ariel Ramirez <ariel12jona@gmail.com>
2024-09-23 22:32:34 -0600

    mejor

--- resources/views/escuela/profesor/show.blade.php
+++ resources/views/escuela/profesor/show.blade.php
@@ -14,19 +14,27 @@
 
          <div class="card">
             <div class="card-body">
-               <h4 class="card-title">{{$g->nombre}} {{$g->email}}</h4>
+
+
+               <div class="w-100"></div>
+
+               <div class="row">
+                  <div class="col">
+                     <h4 class=" h4">{{$g->nombre}} {{$g->email}} </h4>
+                  </div>
+               </div>
 
 
 
                <div class="row">
                   <div class="col-3">
+
                      <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                         <button class="nav-link active" id="v-pills-home-tab" data-toggle="pill" data-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">ACTUALIZAR</button>
                         <button class="nav-link" id="v-pills-profile-tab" data-toggle="pill" data-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">DATOS</button>
-
-
                      </div>
                   </div>
+
                   <div class="col-9">
                      <div class="tab-content" id="v-pills-tabContent">
                         <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
