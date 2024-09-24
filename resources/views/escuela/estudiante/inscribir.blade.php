a546215e74f4b0758c94244aeb5cef3e39520fd9
Ariel Ramirez <ariel12jona@gmail.com>
2024-09-24 00:14:56 -0600

    mejoras de sistema

--- resources/views/escuela/estudiante/inscribir.blade.php
+++ resources/views/escuela/estudiante/inscribir.blade.php
@@ -18,46 +18,53 @@
                <form action="{{route('estudiante.store')}}" method="post">
                   @csrf
 
-                  <div class="form-group">
-                     <label for="">NOMBRE</label>
-                     <input type="text" class="form-control" name="nombre">
-                  </div>
+                  <div class="row">
+                     <div class="col">
+                        <div class="form-group">
+                           <label for="">NOMBRE</label>
+                           <input type="text" class="form-control" name="nombre">
+                        </div>
 
-                  <div class="form-group">
-                     <label for="">APELLIDO</label>
-                     <input type="text" class="form-control" name="apellido">
-                  </div>
+                        <div class="form-group">
+                           <label for="">APELLIDO</label>
+                           <input type="text" class="form-control" name="apellido">
+                        </div>
 
-                  <div class="form-group">
-                     <label for="">CUI</label>
-                     <input type="text" class="form-control" name="cui">
-                  </div>
+                        <div class="form-group">
+                           <label for="">CUI</label>
+                           <input type="text" class="form-control" name="cui">
+                        </div>
 
-                  <div class="form-group">
-                     <label for="">TELEFONO</label>
-                     <input type="text" class="form-control" name="telefono">
-                  </div>
+                        <div class="form-group">
+                           <label for="">TELEFONO</label>
+                           <input type="text" class="form-control" name="telefono">
+                        </div>
+                     </div>
+                     <div class="col">
+                        <div class="form-group">
+                           <label for="">PADRE</label>
+                           <input type="text" class="form-control" name="padre">
+                        </div>
 
-                  <div class="form-group">
-                     <label for="">PADRE</label>
-                     <input type="text" class="form-control" name="padre">
-                  </div>
+                        <div class="form-group">
+                           <label for="">TELEFONO PADRE</label>
+                           <input type="text" class="form-control" name="t_padre">
+                        </div>
 
-                  <div class="form-group">
-                     <label for="">TELEFONO PADRE</label>
-                     <input type="text" class="form-control" name="t_padre">
-                  </div>
+                        <div class="form-group">
+                           <label for="">CUI PADRE</label>
+                           <input type="text" class="form-control" name="t_cui">
+                        </div>
 
-                  <div class="form-group">
-                     <label for="">CUI PADRE</label>
-                     <input type="text" class="form-control" name="t_cui">
+                        <div class="form-group">
+                           <select name="grado_id" id="grado" class="mt-4">
+                              @foreach ($grados as $p)
+                              <option value="{{$p->id}}">{{$p->nombre}}, Seccion: {{$p->seccion}}</option>
+                              @endforeach
+                           </select>
+                        </div>
+                     </div>
                   </div>
-
-                  <select name="grado_id" id="grado" class="mt-4">
-                     @foreach ($grados as $p)
-                     <option value="{{$p->id}}">{{$p->nombre}}, Seccion: {{$p->seccion}}</option>
-                     @endforeach
-                  </select>
                   <button type="submit" class="btn btn-success btn-block">INSCRIBIR</button>
 
 
